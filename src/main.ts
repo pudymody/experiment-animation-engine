6ffe7ebea01cf910251d3f7476309fb895873e3a
Federico Scodelaro <federicoscodelaro@gmail.com>
2025-10-16 22:08:17 -0300

    feat: Make player a custom element

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,14 @@
 import MyScene from "./customScene";
 import DOMCanvas from "./renderer/domcanvas";
 
+customElements.define("canvas-player", DOMCanvas);
+
 const video = new MyScene;
-const player = new DOMCanvas();
-player.src = video;
+const $player = document.querySelector<DOMCanvas>("canvas-player");
+if ($player !== null) {
+	$player.src = video;
+}
 
 // TODO
 //
-// Make this a webcomponent
 // Make exporter
-document.querySelector("#preview")?.prepend(player.$canvas);
-document.querySelector("#play")?.addEventListener("click", function(_) {
-	if (player.isPlaying) {
-		player.pause();
-	} else {
-		player.play();
-	}
-})
-document.querySelector("#stop")?.addEventListener("click", function(_) {
-	player.reset();
-})
-
-const $slider = document.querySelector<HTMLInputElement>("#position");
-if ($slider !== null) {
-	$slider.max = video.endTime.toString();
-	$slider.addEventListener("input", function(_) {
-		player.playTime = $slider.valueAsNumber;
-	})
-	player.addEventListener("currentTime", function(e) {
-		$slider.valueAsNumber = e.detail;
-	})
-}
--- src/renderer/domcanvas.ts
+++ src/renderer/domcanvas.ts
@@ -7,10 +7,12 @@ interface Scene {
 	draw(ctx: CanvasRenderingContext2D): void
 }
 
-export default class DOMCanvas extends EventTarget {
-	public readonly $canvas: HTMLCanvasElement;
+export default class DOMCanvas extends HTMLElement {
+	private $canvas: HTMLCanvasElement;
+	private $range: HTMLInputElement;
 	private ctx: CanvasRenderingContext2D;
 	private _isPlaying: boolean = false;
+	private $shadowRoot: ShadowRoot;
 
 	private start: number | undefined;
 	private _playTime: number = 0;
@@ -20,13 +22,82 @@ export default class DOMCanvas extends EventTarget {
 	constructor() {
 		super();
 
+		this.newFrame = this.newFrame.bind(this);
+		this.$shadowRoot = this.attachShadow({ mode: "closed" });
+
 		this.$canvas = document.createElement("canvas");
 		const ctx = this.$canvas.getContext("2d");
 		if (ctx === null) {
 			throw new Error("could not get context from canvas");
 		}
 		this.ctx = ctx;
-		this.newFrame = this.newFrame.bind(this);
+		this.$shadowRoot.appendChild(this.$canvas);
+
+		this._buildUI();
+	}
+
+	private _buildUI() {
+		const $style = document.createElement("style");
+		$style.innerHTML = `
+			:host {
+				display: block;
+				width: 100%;
+				height: 100%;
+				box-sizing: border-box;
+				border: 1px solid #2a2b2b;
+			}
+
+			canvas {
+				display: block;
+				width: 100%;
+			}
+
+			div {
+				display: flex;
+				box-sizing: border-box;
+				gap: 0.5rem;
+				padding: 0.5rem;
+				background: #2a2b2b;
+			}
+
+			input {
+				margin: 0;
+				flex: 1;
+			}
+		`;
+
+		const $playButton = document.createElement("button");
+		$playButton.innerHTML = "Play/Pause";
+		$playButton.addEventListener("click", () => this.toggle());
+
+		const $stopButton = document.createElement("button");
+		$stopButton.innerHTML = "Stop";
+		$stopButton.addEventListener("click", () => this.reset());
+
+		const $range = document.createElement("input");
+		$range.type = "range";
+		$range.min = "0";
+		$range.max = "0";
+		$range.addEventListener("input", (e) => {
+			this.playTime = (e.target as HTMLInputElement).valueAsNumber;
+		});
+		this.$range = $range;
+
+		const $div = document.createElement("div");
+		$div.appendChild($style);
+		$div.appendChild($playButton);
+		$div.appendChild($stopButton);
+		$div.appendChild($range);
+
+		this.$shadowRoot.appendChild($div);
+	}
+
+	toggle() {
+		if (this.isPlaying) {
+			this.pause();
+		} else {
+			this.play();
+		}
 	}
 
 	get src(): Scene | undefined {
@@ -37,6 +108,7 @@ export default class DOMCanvas extends EventTarget {
 		this.$canvas.width = src.width;
 		this.$canvas.height = src.height;
 		this._src = src;
+		this.$range.max = src.endTime.toString();
 		this.reset();
 	}
 
@@ -80,6 +152,7 @@ export default class DOMCanvas extends EventTarget {
 
 			const currentTimeEvent = new CustomEvent("currentTime", { detail: value });
 			this.dispatchEvent(currentTimeEvent);
+			this.$range.valueAsNumber = value;
 		}
 	}
 

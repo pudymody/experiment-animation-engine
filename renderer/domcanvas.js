6dbc469dc00e614baff49cbbc5a839ffee0d43e5
Federico Scodelaro <federicoscodelaro@gmail.com>
2026-01-15 00:43:24 -0300

    feat: Use icons for buttons

--- renderer/domcanvas.js
+++ renderer/domcanvas.js
@@ -1,4 +1,19 @@
 /** @import { Scene } from 'engine' */
+
+const playButton = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+</svg>`;
+const pauseButton = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+</svg>`;
+const stopButton = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z" />
+</svg>`
+const exportButton = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>`
+
+
 /** @extends HTMLElement */
 export default class DOMCanvas extends HTMLElement {
     /**
@@ -24,6 +39,10 @@ export default class DOMCanvas extends HTMLElement {
      * @private
      */
     $shadowRoot;
+    /**
+     * @private
+     */
+    $playButton;
     /**
      * @private
      */
@@ -92,12 +111,25 @@ export default class DOMCanvas extends HTMLElement {
 				margin: 0;
 				flex: 1;
 			}
+
+			button {
+				all: unset;
+				display: block;
+			}
+
+			svg {
+				width: 1.75rem;
+				color: white;
+				cursor: pointer;
+				display: block;
+			}
 		`;
         const $playButton = document.createElement("button");
-        $playButton.innerHTML = "Play/Pause";
+        $playButton.innerHTML = playButton;
         $playButton.addEventListener("click", () => this.toggle());
+				this.$playButton = $playButton;
         const $stopButton = document.createElement("button");
-        $stopButton.innerHTML = "Stop";
+        $stopButton.innerHTML = stopButton;
         $stopButton.addEventListener("click", () => this.reset());
         this.$range.type = "range";
         this.$range.min = "0";
@@ -106,7 +138,7 @@ export default class DOMCanvas extends HTMLElement {
             this.playTime = e.target.valueAsNumber;
         });
 				const $exportButton = document.createElement("button");
-        $exportButton.innerHTML = "Export";
+        $exportButton.innerHTML = exportButton;
         $exportButton.addEventListener("click", () => this.export());
         const $div = document.createElement("div");
         $div.appendChild($style);
@@ -162,6 +194,7 @@ export default class DOMCanvas extends HTMLElement {
             return;
         }
         this._isPlaying = true;
+				this.$playButton.innerHTML = pauseButton;
         window.requestAnimationFrame(this.newFrame);
     }
     /**
@@ -179,6 +212,7 @@ export default class DOMCanvas extends HTMLElement {
         }
         this._isPlaying = false;
         this.start = undefined;
+				this.$playButton.innerHTML = playButton;
     }
     /**
      * @returns {void}
@@ -187,6 +221,7 @@ export default class DOMCanvas extends HTMLElement {
         this._isPlaying = false;
         this.start = undefined;
         this.playTime = 0;
+				this.$playButton.innerHTML = playButton;
     }
     /**
      * @param {number} value
